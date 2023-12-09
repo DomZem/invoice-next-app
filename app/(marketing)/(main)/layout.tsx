8d2623b042d09a78b3993c5a782b4e619c14f3fc
Dominik <dominik.zemlik.dev@gmail.com>
2023-12-09 13:26:12 +0100

    Create marketing layout and main page

--- app/(marketing)/(main)/layout.tsx
+++ app/(marketing)/(main)/layout.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@/components/ui/Button";
+import ModeToggle from "@/components/ui/ModeToggle";
+import Link from "next/link";
+
 export default function MarketingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <header className="fixed left-0 top-0 h-[72px] w-full bg-midnightBlue md:h-20">
+        <div className="mx-auto flex h-full max-w-7xl items-center justify-between px-2">
+          <h1 className="heading-l-text text-white">Invoice</h1>
+          <div className="flex items-center gap-4">
+            <ModeToggle />
+            <Button asChild>
+              <Link href="/login">Log in</Link>
+            </Button>
+          </div>
+        </div>
+      </header>
+      {children}
+    </div>
+  );
 }
