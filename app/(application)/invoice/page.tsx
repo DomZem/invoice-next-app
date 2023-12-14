2258aaf77b2184cca9d13f869adcb8d7d1b7d38c
Dominik <dominik.zemlik.dev@gmail.com>
2023-12-15 00:24:35 +0100

    Create logic for fetch invoices

--- app/(application)/invoice/page.tsx
+++ app/(application)/invoice/page.tsx
@@ -1,8 +1,53 @@
+"use client";
+
 import CreateInvoice from "@/components/InvoiceForm/CreateInvoice";
+import { Invoice } from "@/components/InvoiceForm/formSchema";
 import { Button } from "@/components/ui/Button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/Sheet";
+import { axiosInstance } from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { LuLoader2 } from "react-icons/lu";
+
+type FetchInvoice = Invoice & {
+  id: number;
+};
+
+const fetchInvoices = async () => {
+  const response = await axiosInstance.get<FetchInvoice[]>("/invoice", {
+    withCredentials: true,
+  });
+
+  return response.data;
+};
 
 export default function InvoicePage() {
+  const { data, isLoading, error } = useQuery<FetchInvoice[]>({
+    queryFn: fetchInvoices,
+    queryKey: ["invoices"],
+  });
+
+  const router = useRouter();
+
+  if (isLoading) {
+    return (
+      <main className="flex items-center justify-center">
+        <LuLoader2 className="animate-spin text-4xl font-bold text-primary" />
+      </main>
+    );
+  }
+
+  if (error) {
+    // Redirect unauthorized user
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      router.replace("/");
+      return null;
+    } else {
+      return <h1>Something went wrong try maybe later!</h1>;
+    }
+  }
+
   return (
     <main className="mx-auto flex w-full max-w-[730px] flex-col gap-8 overflow-hidden px-6 py-9 md:gap-[55px] md:px-12 md:py-[62px] lg:gap-16 lg:py-[78px]">
       <section className="flex items-center justify-between">
@@ -27,6 +72,14 @@ export default function InvoicePage() {
           </Sheet>
         </div>
       </section>
+
+      {!data?.length ? (
+        <p>Empty invoice</p>
+      ) : (
+        <ul>
+          <li>{data.length}</li>
+        </ul>
+      )}
     </main>
   );
 }
