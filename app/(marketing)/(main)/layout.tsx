a9c929f555fe137cbc460dec14012b5f20a03687
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-16 14:26:16 +0100

    Create marketing layout and home page

--- app/(marketing)/(main)/layout.tsx
+++ app/(marketing)/(main)/layout.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/UI/Button';
+import ModeToggle from '@/components/UI/ModeToggle';
+import Logo from '@/public/logo.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+
 interface MarketingLayoutProps {
   children: React.ReactNode;
 }
@@ -5,8 +11,21 @@ interface MarketingLayoutProps {
 export default function MarketingLayout({ children }: MarketingLayoutProps) {
   return (
     <>
-      <header>
-        <h1>Marketing Layout</h1>
+      <header className="bg-midnightBlue fixed left-0 top-0 h-[72px] w-full md:h-20">
+        <div className="mx-auto flex h-full max-w-7xl items-center justify-between px-2">
+          <div className="flex items-center gap-3">
+            <div className="bg-primary rounded-md p-3">
+              <Image className="w-7 md:w-[30px]" src={Logo} alt="logo" />
+            </div>
+            <h1 className="text-heading-m text-white">Invoice</h1>
+          </div>
+          <div className="flex items-center gap-4">
+            <ModeToggle />
+            <Button asChild>
+              <Link href="/login">Login</Link>
+            </Button>
+          </div>
+        </div>
       </header>
       {children}
     </>
