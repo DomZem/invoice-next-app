914f81a851e62ca610e433c1ad4350ede109d1ee
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-16 20:11:13 +0100

    Create RegisterForm component

--- app/(marketing)/(auth)/register/page.tsx
+++ app/(marketing)/(auth)/register/page.tsx
@@ -1,7 +1,22 @@
+import RegisterForm from '@/components/RegisterForm/RegisterForm';
+import { RegisterType } from '@/components/RegisterForm/formSchema';
+
+const defaultValues: RegisterType = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  avatar: '',
+};
+
 export default function RegisterPage() {
   return (
-    <div>
-      <h2>Register your account!</h2>
-    </div>
+    <section className="w-full max-w-lg rounded-lg bg-white p-6 shadow-wrapper dark:bg-midnightBlue">
+      <h2 className="mb-6 text-heading-m text-starlessNight dark:text-white md:text-heading-l">
+        Register
+      </h2>
+      <RegisterForm defaultValues={defaultValues} />
+    </section>
   );
 }
---
+++ components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,182 @@
+'use client';
+
+import { axiosInstance } from '@/lib/axios';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { LuLoader2 } from 'react-icons/lu';
+import { Button } from '../UI/Button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../UI/Form';
+import { Input } from '../UI/Input';
+import { RegisterType, registerFormSchema } from './formSchema';
+
+const register = async (data: RegisterType) => {
+  return axiosInstance.post('/auth/register', data, {
+    withCredentials: true,
+  });
+};
+
+interface RegisterFormProps {
+  defaultValues: RegisterType;
+}
+
+export default function RegisterForm({ defaultValues }: RegisterFormProps) {
+  const form = useForm<RegisterType>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues,
+  });
+
+  const router = useRouter();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (data: RegisterType) =>
+      toast.promise(register(data), {
+        loading: 'Creating account ...',
+        success: 'Account has been created 🔥',
+        error: (error: Error | AxiosError) => {
+          let message = "Something went wrong. Account hasn't been created";
+
+          if (axios.isAxiosError(error)) {
+            const errorMessage = error.response?.data.message;
+
+            if (errorMessage) {
+              return `${message}. Error: ${errorMessage}`;
+            }
+          }
+
+          return message;
+        },
+      }),
+    onSuccess: () => {
+      router.push('/invoices');
+    },
+  });
+
+  const onSubmit: SubmitHandler<RegisterType> = (data) => {
+    mutate(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <div className="grid grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>first name</FormLabel>
+
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>last name</FormLabel>
+
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>address email</FormLabel>
+
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>password</FormLabel>
+
+              <FormControl>
+                <Input {...field} type="password" />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>confirm password</FormLabel>
+
+              <FormControl>
+                <Input {...field} type="password" />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="avatar"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>avatar link</FormLabel>
+
+              <FormControl>
+                <Input {...field} type="url" />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          className="flex w-full items-center justify-center"
+          type="submit"
+          disabled={isPending}
+        >
+          {isPending && (
+            <LuLoader2 className="mr-2 animate-spin text-base font-bold" />
+          )}
+          {isPending ? 'Processing ...' : 'Submit'}
+        </Button>
+      </form>
+    </Form>
+  );
+}
