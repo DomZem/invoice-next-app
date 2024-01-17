c824ea47aea479a0f55202638abec007449a69b6
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-17 10:09:45 +0100

    Create logic to protect application pages from not logged in users

--- app/(application)/layout.tsx
+++ app/(application)/layout.tsx
@@ -1,10 +1,19 @@
 import AppHeader from '@/components/AppHeader';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 interface ApplicationLayout {
   children: React.ReactNode;
 }
 
 export default function ApplicationLayout({ children }: ApplicationLayout) {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get('access_token');
+
+  if (!accessToken) {
+    redirect('/');
+  }
+
   return (
     <aside className="grid h-screen grid-rows-[72px_1fr] overflow-hidden md:grid-rows-[80px_1fr] lg:grid-cols-[103px_1fr] lg:grid-rows-none">
       <AppHeader />
