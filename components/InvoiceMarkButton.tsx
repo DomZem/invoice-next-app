7092322752db11067be96f6d4e6eca99dbf70058
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-24 21:27:36 +0100

    Extract update invoice status logic into hook from InvoiceMarkButton component

--- components/InvoiceMarkButton.tsx
+++ components/InvoiceMarkButton.tsx
@@ -1,10 +1,7 @@
 'use client';
 
 import { Button } from '@/components/UI/Button';
-import { axiosInstance } from '@/lib/axios';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import axios, { AxiosError } from 'axios';
-import toast from 'react-hot-toast';
+import useUpdateInvoiceStatusMutation from '@/hooks/useUpdateInvoiceStatusMutation';
 import { Status } from './InvoiceForm/formSchema';
 
 interface InvoiceMarkButtonProps {
@@ -12,64 +9,17 @@ interface InvoiceMarkButtonProps {
   status: Status;
 }
 
-const updateInvoiceStatus = async ({
-  id,
-  status,
-}: {
-  id: number;
-  status: Status;
-}) => {
-  return axiosInstance.patch(
-    `/invoice/${id}`,
-    { status },
-    {
-      withCredentials: true,
-    },
-  );
-};
-
 export default function InvoiceMarkButton({
   id,
   status,
 }: InvoiceMarkButtonProps) {
-  const queryClient = useQueryClient();
-
-  const nextStatus: Status =
-    status === 'DRAFT' ? 'PENDING' : status === 'PENDING' ? 'PAID' : 'DRAFT';
-
-  const { mutate, isPending } = useMutation({
-    mutationFn: (data: { id: number; status: Status }) =>
-      toast.promise(updateInvoiceStatus(data), {
-        loading: 'Updating invoice status ...',
-        success: `Invoice status has been updated to ${nextStatus.toLowerCase()} 🔥`,
-        error: (error: Error | AxiosError) => {
-          let message =
-            "Something went wrong. Invoice status hasn't been updated";
-
-          if (axios.isAxiosError(error)) {
-            const errorMessage = error.response?.data.message;
-            if (errorMessage) {
-              return `${message}. Error: ${errorMessage}`;
-            }
-          }
-
-          return message;
-        },
-      }),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['invoices'] });
-    },
-  });
-
-  const handleUpdateInvoiceStatus = () => {
-    mutate({ id, status: nextStatus });
-  };
+  const { mutate, isPending } = useUpdateInvoiceStatusMutation(status);
 
   return (
     <Button
       className="truncate"
       disabled={isPending}
-      onClick={handleUpdateInvoiceStatus}
+      onClick={() => mutate({ id, status })}
     >
       Mark as{' '}
       {status === 'DRAFT' ? 'Pending' : status === 'PENDING' ? 'Paid' : 'Draft'}
---
+++ hooks/useUpdateInvoiceStatusMutation.ts
@@ -0,0 +1,52 @@
+import { Status } from '@/components/InvoiceForm/formSchema';
+import { axiosInstance } from '@/lib/axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
+import toast from 'react-hot-toast';
+
+const updateInvoiceStatus = async ({
+  id,
+  status,
+}: {
+  id: number;
+  status: Status;
+}) => {
+  return axiosInstance.patch(
+    `/invoice/${id}`,
+    { status },
+    {
+      withCredentials: true,
+    },
+  );
+};
+
+export default function useUpdateInvoiceStatusMutation(status: Status) {
+  const queryClient = useQueryClient();
+
+  const nextStatus: Status =
+    status === 'DRAFT' ? 'PENDING' : status === 'PENDING' ? 'PAID' : 'DRAFT';
+
+  return useMutation({
+    mutationFn: (data: { id: number; status: Status }) =>
+      toast.promise(updateInvoiceStatus({ ...data, status: nextStatus }), {
+        loading: 'Updating invoice status ...',
+        success: `Invoice status has been updated to ${nextStatus.toLowerCase()} 🔥`,
+        error: (error: Error | AxiosError) => {
+          let message =
+            "Something went wrong. Invoice status hasn't been updated";
+
+          if (axios.isAxiosError(error)) {
+            const errorMessage = error.response?.data.message;
+            if (errorMessage) {
+              return `${message}. Error: ${errorMessage}`;
+            }
+          }
+
+          return message;
+        },
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['invoices'] });
+    },
+  });
+}
