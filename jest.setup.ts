2b4fec76cc2a2288dc308fd5c326f4148500bf3b
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-24 19:55:20 +0100

    Create integration tests for LogoutButton component

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,3 +4,17 @@ import { server } from './mocks/server';
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+Object.defineProperty(global, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
