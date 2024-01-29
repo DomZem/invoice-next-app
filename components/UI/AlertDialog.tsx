6ffc85698d92b74377dbcb137bf422c0952219da
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-29 12:56:49 +0100

    Change width of AlertDialog component on mobile view

--- components/UI/AlertDialog.tsx
+++ components/UI/AlertDialog.tsx
@@ -17,7 +17,7 @@ const AlertDialogOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Overlay
     className={cn(
-      'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black bg-opacity-50 backdrop-blur-sm',
+      'fixed inset-0 z-50 bg-black bg-opacity-50 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
       className,
     )}
     {...props}
@@ -36,7 +36,7 @@ const AlertDialogContent = React.forwardRef<
     <AlertDialogPrimitive.Content
       ref={ref}
       className={cn(
-        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid w-full max-w-[480px] translate-x-[-50%] translate-y-[-50%] gap-[22px] rounded-lg bg-white p-8 shadow-wrapper duration-200 dark:bg-midnightBlue',
+        'fixed left-[50%] top-[50%] z-50 grid w-[calc(100%-24px)] max-w-[480px] translate-x-[-50%] translate-y-[-50%] gap-[22px] rounded-lg bg-white p-8 shadow-wrapper duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] dark:bg-midnightBlue md:w-full',
         className,
       )}
       {...props}
