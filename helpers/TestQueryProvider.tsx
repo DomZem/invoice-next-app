f210a5a9142556f3384f1ad22f178c253a177d82
DomZem <dominik.zemlik.dev@gmail.com>
2024-12-19 21:28:56 +0100

    Add deploy job

--- helpers/TestQueryProvider.tsx
+++ helpers/TestQueryProvider.tsx
@@ -1,18 +1,19 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-    },
-  },
-});
-
 export const TestQueryProvider = ({
   children,
 }: {
   children: React.ReactNode;
-}) => (
-  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-);
+}) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
